40877494fee73d9c2d786c970bc7b326af90e88b
Kevin Duzguner <67980369+kevinbaddata@users.noreply.github.com>
2022-02-23 13:45:38 +0100

    Commited

---
+++ DB-Lib/database/db.php
@@ -0,0 +1,52 @@
+<?php
+class DB
+{
+    static $db;
+
+    public static function Connect()
+    {
+
+        try {
+            self::$db = new PDO("mysql:host=localhost;dbname=oop", 'root', '');
+            // set the PDO error mode to exception
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "DB Connection successfull!";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public static function CreateDB($param = '')
+    {
+
+        try {
+            self::$db->exec("CREATE DATABASE " . $param);
+            echo "Database created successfully <br>";
+        } catch (PDOException $e) {
+
+            //...
+        }
+        //...
+
+
+    }
+
+    public static function Insert($table, $params = [])
+    {
+        $keys = '';
+        $values = [];
+
+        foreach ($params as $key => $item) {
+            $keys .= $key . ' = ? , ';
+            array_push($values, $item);
+        }
+
+        $keys = trim($keys, ' , ');
+
+        echo 'Successfully added <br>';
+        return self::$db->prepare('INSERT INTO ' . $table . ' SET ' . $keys . ' ')
+            ->execute(
+                $values
+            );
+    }
+}
